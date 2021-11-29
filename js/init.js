c4f680f4f229b4eb401d612ce1e1ad688ca300c4
angelauthweb <94419844+angelauthweb@users.noreply.github.com>
2021-11-28 19:06:35 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyC4ZvBbdhtTllHKduyj-A31UrUoFqlzAL4",
-  authDomain: "chat3cv51jarn.firebaseapp.com",
-  projectId: "chat3cv51jarn",
-  storageBucket: "chat3cv51jarn.appspot.com",
-  messagingSenderId: "738242122378",
-  appId: "1:738242122378:web:95605cdd1616f02be19373",
-  measurementId: "G-SY2QFHHP5B"
+  apiKey: "AIzaSyBbmmhnrKQJD6kEuM_hEB-JJKhdaaRQkDQ",
+  authDomain: "cloud-storage-caa28.firebaseapp.com",
+  projectId: "cloud-storage-caa28",
+  storageBucket: "cloud-storage-caa28.appspot.com",
+  messagingSenderId: "1001554925225",
+  appId: "1:1001554925225:web:f24f781109ec18bcb1bbb8",
+  measurementId: "G-8YZ46G9ZF1"
 });
